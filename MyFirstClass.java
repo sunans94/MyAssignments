82b1e4bb72e9a255f3480474b6e789ee4a772ef3
sunans94 <114824480+sunans94@users.noreply.github.com>
2022-10-01 22:39:27 +0530

    Add files via upload
    day 1

---
+++ MyFirstClass.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class MyFirstClass {
+
+	int a = 5;
+	byte wheel = 4;
+	short mileage = 32767;
+	long distance = 123456789L;
+	float engine = 12345.12345F;
+	double driver = 123456789.123456789;
+	char car = 'v';
+	boolean drive = true;
+	String name = "verna";
+	
+}
